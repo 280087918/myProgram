c36dd125b6aa89cfbec4667248484eeba14c9aac
280087918 <280087918@qq.com>
2017-05-24 09:37:15 +0800

    项目目录结构移动

--- j2ee/spring-mysql-readwrite/src/main/resources/sql/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `tbl_user` (
-  `id` varchar(32) NOT NULL DEFAULT '',
-  `name` varchar(32) DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- j2ee/spring-transaction/src/main/java/com/john/sfu/service/SFUService.java
+++
@@ -1,9 +0,0 @@
-package com.john.sfu.service;
-
-import com.john.sfu.dto.SFUDto;
-
-public interface SFUService {
-	public void update(SFUDto udDto, long time);
-	
-	public void updateReturn(SFUDto udDto, boolean isReturn);
-}
